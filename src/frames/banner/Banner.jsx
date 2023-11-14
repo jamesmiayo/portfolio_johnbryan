1bcc889c31b323113183c9a5358342bda9d11ff7
jamesmiayo <35914986+jamesmiayo@users.noreply.github.com>
2023-11-14 23:40:22 +0800

    banner

--- src/frames/banner/Banner.jsx
+++ src/frames/banner/Banner.jsx
@@ -5,7 +5,15 @@ import banner from '../../images/arch.jpg';
 const Banner = () => {
   return (
     <div className="container container__banner">
-      <img src={banner} alt="banner" className="img__banner" />
+      <div className="container__imgbanner">
+        <img src={banner} alt="banner" className="img__banner" />
+      </div>
+      <a href="#contact" className="btn btn-primary btn__contact">
+        CONTACT ME
+      </a>
+      <a href="#contact" className="btn btn-secondary btn__about">
+        Message me
+      </a>
     </div>
   );
 };
