9ad6f82e5a4072523a45ae027005ac96b3c7bc72
jamesmiayo <35914986+jamesmiayo@users.noreply.github.com>
2023-11-15 22:58:41 +0800

    update interest

--- src/frames/interest/Interest.jsx
+++ src/frames/interest/Interest.jsx
@@ -18,13 +18,9 @@ const Interest = () => {
               ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make a type specimen book. It has
               survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
+              electronic typesetting 300+ character lang
             </small>
-            <a href="#contact" className="btn btn-secondary">
+            <a href="#projects" className="btn btn-secondary">
               PROJECTS
             </a>
           </div>
@@ -39,13 +35,9 @@ const Interest = () => {
               ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make a type specimen book. It has
               survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
+              electronic typesetting 300+ character lang
             </small>
-            <a href="#contact" className="btn btn-primary">
+            <a href="#works" className="btn btn-primary">
               WORKS
             </a>
           </div>
